958e3096997f30a7ade25cd0d7da46881eeeaca7
Sybille Cordeau <sybillecordeau@gmail.com>
2018-05-21 19:10:31 +0200

    seed movies genres + some pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,89 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+{
+  "genres": [
+    {
+      "id": 28,
+      "name": "Action",
+      "picture":
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy",
+      "picture": "theatre-masks.png"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family",
+      "picture": "kids-couple.png"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy",
+      "picture": "japanese-dragon.png"
+    },
+    {
+      "id": 36,
+      "name": "History",
+      "picture": "sword.png"
+    },
+    {
+      "id": 27,
+      "name": "Horror",
+      "picture": "human-skull"
+    },
+    {
+      "id": 10402,
+      "name": "Music",
+      "picture": "music-player.png"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery",
+      "picture": "halloween-ghost.png"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War",
+      "picture": "gun.png"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+}
