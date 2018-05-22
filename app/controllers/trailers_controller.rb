4da141ee86a7fd914aec2540376fb1e679dff9c9
Romain Grossard <rgrossard@gmail.com>
2018-05-22 19:25:52 +0200

    les filters marchent reste les releases_dates

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -3,8 +3,11 @@ require 'tmdb/tmdb'
 class TrailersController < ApplicationController
   def index
     # aller chercher les genres dans les cookies
-    @movies = TMDB::Discover.movie({ genre_ids: [28, 80, 18] })
-    @movies[0] = TMDB::Movie.details(@movies.first['id'])
+    discover_options = {}
+    discover_options[:genre_ids] = JSON.parse(cookies[:genre_ids]) if cookies[:genre_ids].present?
+    discover_options[:moment] = cookies[:moment]
 
+    @movies = TMDB::Discover.movie(discover_options)
+    @movies[0] = TMDB::Movie.details(@movies.first['id'])
   end
 end
--- lib/tmdb/discover.rb
+++ lib/tmdb/discover.rb
@@ -1,7 +1,11 @@
 module TMDB
   class Discover
     def self.movie(options = {})
-      url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_KEY']}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      genres_options = options[:genre_ids].present? ? "&with_genres=#{options[:genre_ids].join(",")}" : ""
+      url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_KEY']}&language=en-US#{genres_options}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      puts "=" * 20
+      puts url
+      puts "=" * 20
       api_data = RestClient.get(url)
       datas = JSON.parse(api_data)
       movies = datas['results']
