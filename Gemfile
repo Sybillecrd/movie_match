7b172d202929c1309879953f79243683e4b723d0
charles-henri poniard <c.poniard@gmail.com>
2018-05-22 20:09:05 +0200

    init carousel

--- Gemfile
+++ Gemfile
@@ -16,7 +16,6 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
-
 gem 'rest-client'
 
 
--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -7,8 +7,6 @@ class FiltersController < ApplicationController
   def create
     cookies[:genre_ids] = params[:filter][:genre_ids].reject { |g| g.blank? }
     cookies[:moment] = params[:filter][:moment]
-
-    raise
     redirect_to trailers_path
   end
 end
