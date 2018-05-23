368ccc19ba46a09b87ad53284ef9eeb2d8173031
charles-henri poniard <c.poniard@gmail.com>
2018-05-23 17:16:59 +0200

    create or find Movie, create Opinion, find next_movie

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,3 +1,5 @@
+require 'tmdb/tmdb'
+
 class OpinionsController < ApplicationController
 
   # def new
@@ -5,15 +7,19 @@ class OpinionsController < ApplicationController
   # end
 
   def create
-
-    @opinion = Opinion.new(params_opinion)
-    @opinion.save
-
+    @movie = Movie.find_or_create_by(tmdb_id: params_taste["tmdb_id"]) do |movie|
+      movie_info = TMDB::Movie.details(movie[:tmdb_id])
+      movie.title = movie_info["title"]
+      movie.poster = movie_info["poster_path"]
+      movie.release_date = movie_info["release_date"]
+    end
+    @opinion = Opinion.create(movie_id: @movie.id, user_id: current_user.id, value: params_taste["value"])
+    @next_movie = TMDB::Movie.details(params_taste["next_tmdb_id"])
   end
 
   private
 
-  def params_opinion
+  def params_taste
       params.require(:opinion).permit(:value, :tmdb_id, :next_tmdb_id )
   end
 
