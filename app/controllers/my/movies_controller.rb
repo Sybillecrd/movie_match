9bdd9640ac2c1514014edfe84bb9ac4628264c03
Romain Grossard <rgrossard@gmail.com>
2018-05-23 17:45:14 +0200

    tmdb_id + index

--- app/controllers/my/movies_controller.rb
+++ app/controllers/my/movies_controller.rb
@@ -3,6 +3,6 @@ class My::MoviesController < ApplicationController
     # get user_id
     # if user opinion: true => get movie_id
    @movies = Movie.joins(:opinions).where(opinions: {value: true, user_id: current_user})
-   # @tmdb_ids = matches[0]['tmdb_id']
+   @tmdb_ids = @movies[0]['tmdb_id']
   end
 end
