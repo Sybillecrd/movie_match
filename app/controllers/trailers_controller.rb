38fdc6d224c705f099222a8226ada749bde09f9b
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-24 16:11:02 +0200

    added an error message if there is no movie matching

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -7,8 +7,12 @@ class TrailersController < ApplicationController
     discover_options[:genre_ids] = JSON.parse(cookies[:genre_ids]) if cookies[:genre_ids].present?
     discover_options[:moment] = cookies[:moment]
 
-    @movies = TMDB::Discover.movie(discover_options)
-    @movies[0] = TMDB::Movie.details(@movies.first['id'])
-    @movie = @movies[0]
+    if TMDB::Discover.movie(discover_options).empty?
+      @movies = TMDB::Discover.movie(discover_options)
+    else
+      @movies = TMDB::Discover.movie(discover_options)
+      @movies[0] = TMDB::Movie.details(@movies.first['id'])
+      @movie = @movies[0]
+    end
   end
 end
