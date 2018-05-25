75519ab6e60f5da82e9372ddd18d3c0ee7e4f8f6
charles-henri poniard <c.poniard@gmail.com>
2018-05-25 15:13:39 +0200

    redirect home if cookies else redirect to filters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: "filters#new"
+  root to: "trailers#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :filters, only: [:new, :create]
   resources :trailers, only: :index
