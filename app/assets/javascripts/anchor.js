0afee58c4a947d865c28dc61fbfab7c45d322e40
Romain Grossard <rgrossard@gmail.com>
2018-05-28 16:48:38 +0200

    added scroll down when choosing a moment

---
+++ app/assets/javascripts/anchor.js
@@ -0,0 +1,14 @@
+var radioButtons = document.querySelectorAll("[type='radio'][name='filter[moment]']");
+
+
+radioButtons.forEach(function(radioButton) {
+  radioButton.addEventListener("click", function() {
+    setTimeout(function() {
+      window.scroll({
+        top: window.innerHeight,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }, 300);
+  });
+});
