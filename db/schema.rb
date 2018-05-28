972b3a19dcfa597d64a860d9527c34ad18af41bc
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-28 12:15:22 +0200

    added a column release_mail to opinions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_091744) do
+ActiveRecord::Schema.define(version: 2018_05_28_101218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_091744) do
     t.boolean "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "release_mail", default: false
     t.index ["movie_id"], name: "index_opinions_on_movie_id"
     t.index ["user_id"], name: "index_opinions_on_user_id"
   end
