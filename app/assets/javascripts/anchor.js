748eb62eab80b03c33725a447513949e29d6afb0
charles-henri poniard <c.poniard@gmail.com>
2018-05-29 11:53:57 +0200

    comment anchor.js, create radio-button

--- app/assets/javascripts/anchor.js
+++ app/assets/javascripts/anchor.js
@@ -1,14 +1,14 @@
-var radioButtons = document.querySelectorAll("[type='radio'][name='filter[moment]']");
+// var radioButtons = document.querySelectorAll("[type='radio'][name='filter[moment]']");
 
 
-radioButtons.forEach(function(radioButton) {
-  radioButton.addEventListener("click", function() {
-    setTimeout(function() {
-      window.scroll({
-        top: window.innerHeight,
-        left: 0,
-        behavior: 'smooth'
-      });
-    }, 300);
-  });
-});
+// radioButtons.forEach(function(radioButton) {
+//   radioButton.addEventListener("click", function() {
+//     setTimeout(function() {
+//       window.scroll({
+//         top: window.innerHeight,
+//         left: 0,
+//         behavior: 'smooth'
+//       });
+//     }, 300);
+//   });
+// });
