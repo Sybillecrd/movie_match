d36d96c8be929c051cd0e9f38491c19e85430476
Romain Grossard <rgrossard@gmail.com>
2018-05-29 16:11:22 +0200

    call api allociné for moviecode and GET movie showtime list

--- app/controllers/my/movies_controller.rb
+++ app/controllers/my/movies_controller.rb
@@ -1,3 +1,5 @@
+require 'allocine'
+
 class My::MoviesController < ApplicationController
   def index
     # get user_id
@@ -14,6 +16,13 @@ class My::MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @opinion = Opinion.find_by(movie_id: @movie[:id])
     @movie_tmdb = TMDB::Movie.details(@movie.tmdb_id)
+    allocine = Allocine.new
+    allocine_movie_raw = allocine.api(:search, {q: @movie['title']})
+    allocine_movie_parsed = JSON.parse allocine_movie_raw
+    allocine_movie_code = allocine_movie_parsed["feed"]["movie"][0]["code"]
+    @a = allocine.api(:showtimelist, {movie: allocine_movie_code, lat: '49.451827', long: '1.101424', radius: '20', date: '2018-05-29' })
+
+
   end
 
 end
