6285d768d81585f8341b219e7705596d6e98b2f8
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-30 09:42:55 +0200

    determined the date of Today for the showtime list search

--- app/controllers/my/movies_controller.rb
+++ app/controllers/my/movies_controller.rb
@@ -23,7 +23,7 @@ class My::MoviesController < ApplicationController
       allocine_movie_parsed = JSON.parse allocine_movie_raw
       allocine_movie_code = allocine_movie_parsed["feed"]["movie"][0]["code"]
 
-      theaters_showtimes_raw = allocine.api(:showtimelist, {movie: allocine_movie_code, lat: '49.451827', long: '1.101424', radius: '20', date: '2018-05-29' })
+      theaters_showtimes_raw = allocine.api(:showtimelist, {movie: allocine_movie_code, lat: '49.451827', long: '1.101424', radius: '20', date: Date.today.strftime("%Y-%m-%d") })
       @theaters_showtimes_parsed = JSON.parse theaters_showtimes_raw
       if @theaters_showtimes_parsed["feed"]["totalResults"] != 0
         @theaters_showtimes = @theaters_showtimes_parsed['feed']["theaterShowtimes"].first(3)
